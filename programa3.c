1fd190e60fe605c2a422527dc61896fbc994ad6e
GleicianyO <gleicianyop@gmail.com>
2025-06-17 16:03:13 -0300

    Update programa3.c

--- programa3.c
+++ programa3.c
@@ -2,16 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 
 typedef enum { t_int, t_float, t_bool, t_string, t_invalido} TipoDeSensores;
 
 
-TipoDeSensores conversão_para_valor_equivalentes(const char *str){
-    if (strcmp(str, "int") == 0) return t_int;
-    if (strcmp(str, "float") == 0) return t_float;
-    if (strcmp(str, "bool") == 0) return t_bool;
-    if (strcmp(str, "string") == 0) return t_string;
+int verificar_se_inteiro(const char *str) {
+    for (int i = 0; str[i]; i++)
+        if (!isdigit(str[i])) return 0;
+    return 1;
+}
+
+
+TipoDeSensores conversao_para_valor_equivalentes(const char *str) {
+    if (strcmp(str, "CONJ_Z") == 0) return t_int;
+    if (strcmp(str, "CONJ_Q") == 0) return t_float;
+    if (strcmp(str, "BINARIO") == 0) return t_bool;
+    if (strcmp(str, "TEXTO") == 0) return t_string;
 
     return t_invalido;
 }
@@ -52,19 +60,31 @@ int ler_timestamps(int argc, char *argv[], time_t *t_inicio, time_t *t_fim) {
         return 0;
     }
 
+    for (int i = 1; i <= 12; i++) {
+        if (!verificar_se_inteiro(argv[i])) {
+            fprintf(stderr, "Argumento inválido para data/hora: '%s'\n", argv[i]);
+            return 0;
+        }
+    }
 
     int dia_inicio = atoi(argv[1]), mes_inicio = atoi(argv[2]), ano_inicio = atoi(argv[3]);
     int hr_inicio = atoi(argv[4]), min_inicio = atoi(argv[5]), seg_inicio = atoi(argv[6]);
     int dia_fim = atoi(argv[7]), mes_fim = atoi(argv[8]), ano_fim = atoi(argv[9]);
     int hr_fim = atoi(argv[10]), min_fim = atoi(argv[11]), seg_fim = atoi(argv[12]);
 
+    if (dia_inicio < 1 || dia_inicio > 31 || mes_inicio < 1 || mes_inicio > 12 ||
+        hr_inicio < 0 || hr_inicio > 23 || min_inicio < 0 || min_inicio > 59 || seg_inicio < 0 || seg_inicio > 59 ||
+        dia_fim < 1 || dia_fim > 31 || mes_fim < 1 || mes_fim > 12 ||
+        hr_fim < 0 || hr_fim > 23 || min_fim < 0 || min_fim > 59 || seg_fim < 0 || seg_fim > 59) {
+        fprintf(stderr, "\nDatas ou horários fora do intervalo válido.\n");
+        return 0;
+    }
 
     *t_inicio = criar_timestamp(dia_inicio, mes_inicio, ano_inicio, hr_inicio, min_inicio, seg_inicio);
     *t_fim = criar_timestamp(dia_fim, mes_fim, ano_fim, hr_fim, min_fim, seg_fim);
 
-
     if (*t_inicio == (time_t)-1 || *t_fim == (time_t)-1 || *t_fim < *t_inicio) {
-        fprintf(stderr, "\nDatas apresentadas são inválidas.\n");
+        fprintf(stderr, "\nDatas apresentadas são inválidas ou em ordem incorreta.\n");
         return 0;
     }
 
@@ -73,16 +93,48 @@ int ler_timestamps(int argc, char *argv[], time_t *t_inicio, time_t *t_fim) {
 
 
 int ler_sensores(int argc, char *argv[], int *num_sensores, char sensores[][17], TipoDeSensores tipos[]) {
-    *num_sensores = (argc - 13) / 2;
-    if (*num_sensores <= 0) return 0;
+    int args_restantes = argc - 13;
 
-    for (int i = 0; i < *num_sensores; i++) {
-        strncpy(sensores[i], argv[13 + i * 2], 16);
-        sensores[i][16] = '\0';
-
-        tipos[i] = conversão_para_valor_equivalentes(argv[14 + i * 2]);
-        if (tipos[i] == t_invalido) return -1;
+    if (args_restantes <= 0) {
+        fprintf(stderr, "Informe ao menos um sensor válido: CONJ_Z, CONJ_Q, BINARIO, TEXTO\n");
+        return 0;
     }
+
+    if (args_restantes > 4) {
+        fprintf(stderr, "Número máximo de sensores permitidos: 4\n");
+        return 0;
+    }
+
+    const char *sensores_validos[] = {"CONJ_Z", "CONJ_Q", "BINARIO", "TEXTO"};
+    TipoDeSensores tipos_validos[] = {t_int, t_float, t_bool, t_string};
+    int usados[4] = {0, 0, 0, 0};  
+
+    *num_sensores = 0;
+
+    for (int i = 0; i < args_restantes; i++) {
+        const char *sensor = argv[13 + i];
+        int valido = 0;
+        for (int j = 0; j < 4; j++) {
+            if (strcmp(sensor, sensores_validos[j]) == 0) {
+                if (usados[j]) {
+                    fprintf(stderr, "Invalido, um sensor está repetido: %s\n", sensor);
+                    return -1;
+                }
+                usados[j] = 1;
+                strncpy(sensores[*num_sensores], sensor, 16);
+                sensores[*num_sensores][16] = '\0';
+                tipos[*num_sensores] = tipos_validos[j];
+                (*num_sensores)++;
+                valido = 1;
+                break;
+            }
+        }
+        if (!valido) {
+            fprintf(stderr, "Sensor inválido: %s. Sensores válidos: CONJ_Z, CONJ_Q, BINARIO, TEXTO\n", sensor);
+            return -1;
+        }
+    }
+
     return 1;
 }
 
@@ -125,7 +177,7 @@ int main(int argc, char *argv[]) {
     time_t t_inicio, t_fim;
 
     if (!ler_timestamps(argc, argv, &t_inicio, &t_fim)) {
-        printf("\n\n\nPor favor use esse formato: %s dd mm aaaa hh mm ss dd mm aaaa hh mm ss sensor1 tipo1 [sensor2 tipo2 ...]\n\n\n", argv[0]);
+        printf("\n\n\nPor favor use esse formato: %s dd mm aaaa hh mm ss dd mm aaaa hh mm ss sensor1 [sensor2...]\n\n\n", argv[0]);
         return 1;
     }
 
@@ -138,7 +190,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Por favor informe pelo menos um sensor e tipo.\n");
         return 1;
     } else if (res == -1) {
-        fprintf(stderr, "Tipo inválido informado para algum sensor.\n");
+        // Mensagem já foi mostrada na ler_sensores
         return 1;
     }
 
@@ -155,6 +207,4 @@ int main(int argc, char *argv[]) {
     fclose(arquivo);
     printf("Arquivo com o nome 'leituras.txt' gerado com sucesso.\n");
     return 0;
-}
-
-
+} 
